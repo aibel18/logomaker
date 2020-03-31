bdabfe0d849a7d4f8a4642a26244d15b892e2745
Abel-linux <jaticona@gmail.com>
2020-03-31 13:07:45 -0300

    first interface, binding input text

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const MDCRipple = mdc.ripple.MDCRipple;
 const MDCSelect = mdc.select.MDCSelect;
+const MDCTextField = mdc.textField.MDCTextField;
+
 const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar
 // select.listen('MDCSelect:change', () => {
 //   console.log(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
@@ -10,7 +12,8 @@ const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Testing Vue with Material Components'
+    message: 'Testing Vue with Material Components',
+    organization: ""
   },
   mounted: function () {
     console.log("init app");
@@ -20,6 +23,10 @@ var app = new Vue({
 
     const foos = [].map.call(document.querySelectorAll('.vt-ripple'), function (el) {
       return new MDCRipple(el);
+    });
+
+    const text = [].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
+      return new MDCTextField(el);
     });
 
     const topAppBarElement = document.querySelector('.mdc-top-app-bar');
