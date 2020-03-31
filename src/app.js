54c2f32504942f3bbafc8c9ab8d40bc02af97122
Abel-linux <jaticona@gmail.com>
2020-03-31 11:05:02 -0300

    integrating Material Design with Vue.js and init interface

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const MDCRipple = mdc.ripple.MDCRipple;
 const MDCSelect = mdc.select.MDCSelect;
-
+const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar
 // select.listen('MDCSelect:change', () => {
 //   console.log(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
 // });
 
-document.querySelectorAll('.mdc-select').forEach(b => {
-  MDCSelect.attachTo(b);
-  console.log("jje");
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Testing Vue with Material Components'
+  },
+  mounted: function () {
+    console.log("init app");
+    document.querySelectorAll('.mdc-select').forEach(b => {
+      MDCSelect.attachTo(b);
+    });
+
+    const foos = [].map.call(document.querySelectorAll('.vt-ripple'), function (el) {
+      return new MDCRipple(el);
+    });
+
+    const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+    const topAppBar = new MDCTopAppBar(topAppBarElement);
+  }
 });
 
-const foos = [].map.call(document.querySelectorAll('.vt-ripple'), function (el) {
-  return new MDCRipple(el);
-});
